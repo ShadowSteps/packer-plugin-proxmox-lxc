802e8cd20b017c8d1a0889e1b654f6235e7b70d2
Kiril <trickingmachine@gmail.com>
2023-04-20 21:13:06 +0300

    goreleaser

--- plugin.go
+++ plugin.go
@@ -5,11 +5,13 @@ import (
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
 	"os"
 	"proxmox-lxc/proxmox-lxc"
+	"proxmox-lxc/version"
 )
 
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterBuilder("proxmox-lxc", new(proxmox_lxc.Builder))
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
